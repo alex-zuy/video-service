877e2c564743cee761e7a1607b2716cb7f03fb68
Zuy Alexey <zuy_alexey@mail.ru>
2016-01-01 20:16:16 +0300

    Initial commit

---
+++ src/main/java/org/pirat9600q/App.java
@@ -0,0 +1,13 @@
+package org.pirat9600q;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
