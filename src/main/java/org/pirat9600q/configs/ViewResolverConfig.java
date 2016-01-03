56638ae6ef65ad975f0b6f312c7a47582ca83d52
Zuy Alexey <zuy_alexey@mail.ru>
2016-01-03 17:41:18 +0300

    Separated view resolver configuration into separate class

---
+++ src/main/java/org/pirat9600q/configs/ViewResolverConfig.java
@@ -0,0 +1,31 @@
+package org.pirat9600q.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+public class ViewResolverConfig {
+
+    @Bean
+    public InternalResourceViewResolver configureInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(0);
+        return resolver;
+    }
+
+    @Bean
+    public UrlBasedViewResolver configureUrlViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(2);
+        return resolver;
+    }
+}
